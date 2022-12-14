fe988a962aa7df4e03ff2d8d8d1712dc322f029c
lijobazar <lijobazar@hotmail.com>
2022-12-14 07:03:44 -0800

    Upload task 2

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Python script to export data in the JSON format"""
+import json
+from sys import argv
+from requests import get
+
+
+def json_export():
+    """export json"""
+
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_id = argv[1]
+
+    employee = get('{}/users/{}'.format(base_url, user_id))
+    username = employee.json().get('username')
+
+    tasks = get('{}/todos?userId={}'.format(base_url, user_id))
+    task_list = tasks.json()
+
+    json_dict = {user_id: [{"task": task.get('title'),
+                            "completed": task.get('completed'),
+                            "username": username}
+                           for task in task_list]}
+
+    with open('{}.json'.format(user_id), 'w') as fp:
+        json.dump(json_dict, fp)
+
+
+if __name__ == "__main__":
+    json_export()
